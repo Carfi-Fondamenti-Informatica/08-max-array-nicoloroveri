77d67ac683d7534baa465209010e45d4d951ae90
nicoloroveri <116441124+nicoloroveri@users.noreply.github.com>
2022-12-12 19:27:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include "lib.h"
-
 using namespace std;
-
+float x=0;
 int main(){
-int n=0;
-  cin >> n;
-  float a[n];
-  for(int i=0; i<n;i++){
-    cin >> a[i];
+int d=0; cin >> d; float numero[d];
+  for(int i=0; i<d;i++){
+    cin >> numero[i];
   }
-  cout << maggiore(a,n) <<endl;
-  return 0;
+  for(int i=1; i<d;i++){
+    float n=maggiore(numero[i], numero[i-1]);
+    if (n>x){
+      x=n;
+}
+  }
+cout << x;
+return 0;
 }
